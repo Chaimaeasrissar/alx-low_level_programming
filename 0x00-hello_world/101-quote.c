d85ca44a717337c47d0795b2f727517b551c777f
“Chaimaeasrissar” <chaimaeasrissar@gmail.com>
2023-06-15 13:20:40 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include<unistd.h>
 
 /**
@@ -10,8 +11,7 @@
 */
 
 int main(void)
-{	char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+{	fput("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 
-	write(1. quo. 59);
 	return (1);
 }
