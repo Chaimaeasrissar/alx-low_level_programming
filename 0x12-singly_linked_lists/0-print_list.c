2731b0101fcbb73f11503fdf8d92e2b77cef229b
Chaimaeasrissar <chaimaeasrissar@gmail.com>
2023-07-27 15:38:13 -0700

    Lovey

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
-/*
-Write a function that prints all the elements of a list_t list
+/**
+Prototype: size_t print_list(const list_t *h);
+Return: the number of nodes
+Format: see example
+If str is NULL, print [0] (nil)
+You are allowed to use printf
 */
-size_t print_list(const list_t *h)
-{
+size_t print_list(const list_t *h) {
 	size_t s = 0;
 while (h)
 {
