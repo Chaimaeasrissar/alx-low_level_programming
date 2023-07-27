b2cac0dd0a200de75be3331aa68e7f973ce21cb4
Chaimaeasrissar <chaimaeasrissar@gmail.com>
2023-07-27 15:43:07 -0700

    Lovey

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,8 +6,9 @@
  *
  * Return: the number of nodes printed
 */
-size_t print_list(const list_t *h) {
-	size_t s = 0;
+size_t print_list(const list_t *h)
+{
+	 size_t s = 0;
 while (h)
 {
 	if (!h->str)
