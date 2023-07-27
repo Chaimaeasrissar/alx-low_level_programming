2c8064de1f744d6465cad049686a561ca8851155
Chaimaeasrissar <chaimaeasrissar@gmail.com>
2023-07-27 14:35:03 -0700

    lovey

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,14 +2,20 @@
 #include "lists.h"
 
 
-size_t print_list(const list_t *h){
-         size_t count = 0;  //it count the size of the list
-         while (h){
-                 if (h->str == NULL)
-                         printf("[0] (nil)\n");
-                 else
-                         printf("[%u] %s\n", h->len, h->str);h = h->next;
-                 count++;
-         }
-         return (count);
+size_t print_list(const list_t *h)
+{
+	/* It count the size of the list*/
+size_t count = 0;
+while (h)
+{
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	count++;
+	}
+}
+return (count);
 }
